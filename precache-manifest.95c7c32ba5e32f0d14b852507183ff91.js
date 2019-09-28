e69f1eda4080600a51df1d0ef253b0f985ccb02d
Matthew Judge <matthew.judge@bbc.co.uk>
2019-09-28 14:59:57 +0100

    Updates

---
+++ precache-manifest.95c7c32ba5e32f0d14b852507183ff91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dab3794ea242f50af889a0374413059",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46ec3fc2eacb16fc4d5a",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "486eeee75ad90c91cb23",
+    "url": "/static/js/2.e2696517.chunk.js"
+  },
+  {
+    "revision": "46ec3fc2eacb16fc4d5a",
+    "url": "/static/js/main.cc4b5316.chunk.js"
+  },
+  {
+    "revision": "d8989d40d0a8819a757c",
+    "url": "/static/js/runtime-main.fa42786e.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
