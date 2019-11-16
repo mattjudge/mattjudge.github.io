eff44f639f6a2abe1d651e01569cabb800ebd784
Matthew Judge <matthew.judge@bbc.co.uk>
2019-11-16 21:39:16 +0000

    initial source files

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>mattjudge.uk</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,6 +16,15 @@ function App() {
         >
           Learn React
         </a>
+      </header> */}
+      <header className="App-header">
+        <div className="cmd-line">
+          <span className="prompt-user">mattjudge</span>
+          <span className="prompt-colon">:</span>
+          <span className="prompt-dir">~</span>
+          <span className="prompt-dollar">$</span>
+          <input autoFocus className="cmd-input" />
+        </div>
       </header>
     </div>
   );
